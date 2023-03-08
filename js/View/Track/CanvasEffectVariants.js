c365a37e6311c6f6d5e310edea3b59d27ac5707a
Junli Zhang <zhjl86@gmail.com>
2023-03-08 10:45:49 -0800

    add support to VEP CSQ tag

--- js/View/Track/CanvasEffectVariants.js
+++ js/View/Track/CanvasEffectVariants.js
@@ -15,7 +15,7 @@ define([
     ) {
         var variantColorCoding = function(feature) {
             /* console.log(feature); */
-            var value = feature.get('ANN');
+            var value = feature.get('ANN')||feature.get('CSQ');
             if (typeof value !== 'undefined') {
                 var data = value.values;
                 for (var i = 0, len = data.length; i < len; i++) {
@@ -32,6 +32,14 @@ define([
                     } else if (eff_type.indexOf("frameshift_variant") !== -1) {
                         return '#FF0000';
                     } else if (eff_type === "missense_variant") {
+                        if (dataSplit.length > 16){ // default CSQ tag from VEP
+                            // "Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|DISTANCE|STRAND|FLAGS|SYMBOL_SOURCE|HGNC_ID|SIFT"
+                            // ;CSQ=T|missense_variant|MODERATE||TraesCS1A02G001800|Transcript|TraesCS1A02G001800.1|protein_coding|1/2||||192|56|19|A/V|gCc/gTc|||1||||tolerated_low_confidence(0.29)
+                            // get the sift score
+                            let regExp = /\(([^)]+)\)/; // get the string in parentheses
+                            let sift = regExp.exec(dataSplit[dataSplit.length - 1])[1];
+                            if (sift < 0.05) {return "#d30085"}
+                        }
                         return 'purple';
                     } else if (eff_type === "synonymous_variant") {
                         return '#00FF2F';
