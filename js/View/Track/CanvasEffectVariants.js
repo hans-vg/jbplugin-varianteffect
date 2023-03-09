41242f96c432f33c3acf36a54c61d7094d86b7bb
Junli Zhang <zhjl86@gmail.com>
2023-03-09 15:06:46 -0800

    now show the color of the most severe effects no matter orders

--- js/View/Track/CanvasEffectVariants.js
+++ js/View/Track/CanvasEffectVariants.js
@@ -18,37 +18,18 @@ define([
             var value = feature.get('ANN')||feature.get('CSQ');
             if (typeof value !== 'undefined') {
                 var data = value.values;
-                for (var i = 0, len = data.length; i < len; i++) {
-                    counter = i + 1;
-                    /* console.log(value[i]); */
-                    var dataSplit = data[i].split("|");
-                    var eff_type = dataSplit[1];
-                    if (eff_type === "stop_gained") {
-                        return '#FF0000';
-                    } else if (eff_type.indexOf("splice_donor_variant") !== -1) {
-                        return '#FF0000';
-                    } else if (eff_type.indexOf("splice_acceptor_variant") !== -1) {
-                        return '#FF0000';
-                    } else if (eff_type.indexOf("frameshift_variant") !== -1) {
-                        return '#FF0000';
-                    } else if (eff_type === "missense_variant") {
-                        if (dataSplit.length > 16){ // default CSQ tag from VEP
-                            // "Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|DISTANCE|STRAND|FLAGS|SYMBOL_SOURCE|HGNC_ID|SIFT"
-                            // ;CSQ=T|missense_variant|MODERATE||TraesCS1A02G001800|Transcript|TraesCS1A02G001800.1|protein_coding|1/2||||192|56|19|A/V|gCc/gTc|||1||||tolerated_low_confidence(0.29)
-                            // get the sift score
-                            // let regExp = /\(([^)]+)\)/; // get the string in parentheses
-                            // let sift = regExp.exec(dataSplit[dataSplit.length - 1])[1];
-                            // if (sift < 0.05) {return "#d30085"}
-                            let sift = dataSplit[dataSplit.length - 1];
-                            if (sift.includes("deleterious")) {return "#ff9a00"}
-                            
-                        }
-                        return 'purple';
-                    } else if (eff_type === "synonymous_variant") {
-                        return '#00FF2F';
-                    }
+                let all_ann = data.join(',');
+                if (/stop_gained|splice_donor_variant|splice_acceptor_variant|frameshift_variant/.test(all_ann)) {
+                    return '#FF0000'; // red color
+                } else if (all_ann.includes("deleterious")){
+                    return "#ff9a00"; // orange for deleterious missense_variant if using VEP annotated vcf with SIFT score
+                } else if (all_ann.includes("missense_variant")){
+                    return 'purple';
+                } else if (all_ann.includes("synonymous_variant")){
+                    return '#00FF2F'; // green for synonymous variant
+                } else {
+                    return 'blue';
                 }
-                return 'blue'
             } else {
                 return 'blue'
             }
